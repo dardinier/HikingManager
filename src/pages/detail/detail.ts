793644646f4d21945afa190a2e137466937f2dd1
FERRERE Valentin <vferrere0@gmail.com>
2018-05-28 09:33:51 +0200

    testing maps for android

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,64 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {
-    GoogleMaps,
-    GoogleMap,
-    GoogleMapsEvent,
-    GoogleMapOptions,
-    CameraPosition,
-    MarkerOptions,
-    Marker
-} from '@ionic-native/google-maps';
+
+declare var google;
+
 @Component({
     selector: 'page-detail',
     templateUrl: 'detail.html'
 })
+
 export class DetailPage {
-    map: GoogleMap;
-    constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad(){
+    console.info("je suis dkjfdsfd");
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 
-    ionViewDidLoad() {
-        this.loadMap();
-    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-    loadMap() {
-
-        let mapOptions: GoogleMapOptions = {
-            camera: {
-                target: {
-                    lat: 43.0741904,
-                    lng: -89.3809802
-                },
-                zoom: 18,
-                tilt: 30
-            }
-        };
-
-        this.map = GoogleMaps.create('map_canvas', mapOptions);
-
-        // Wait the MAP_READY before using any methods.
-        this.map.one(GoogleMapsEvent.MAP_READY)
-            .then(() => {
-                console.log('Map is ready!');
-
-                // Now you can use all methods safely.
-                this.map.addMarker({
-                    title: 'Ionic',
-                    icon: 'blue',
-                    animation: 'DROP',
-                    position: {
-                        lat: 43.0741904,
-                        lng: -89.3809802
-                    }
-                })
-                    .then(marker => {
-                        marker.on(GoogleMapsEvent.MARKER_CLICK)
-                            .subscribe(() => {
-                                alert('clicked');
-                            });
-                    });
-
-            });
-    }
+  }
 }
+
