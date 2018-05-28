de1ce7328ec238598688d17f226ef5a848365396
FERRERE Valentin <vferrere0@gmail.com>
2018-05-28 11:15:31 +0200

    working google maps

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,36 +1,13 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-declare var google;
-
 @Component({
     selector: 'page-detail',
     templateUrl: 'detail.html'
 })
 
 export class DetailPage {
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
-
   constructor(public navCtrl: NavController) {}
 
-  ionViewDidLoad(){
-    console.info("je suis dkjfdsfd");
-    this.loadMap();
-  }
-
-  loadMap(){
-
-    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
-
-    let mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-  }
 }
 
