d9e8322365fc76c2228b8639347eec62b16891f9
Alexis Dardinier <alexis.dardinier@gmail.com>
2018-06-18 10:48:51 +0200

    Added Google Maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import {NgModule, ErrorHandler} from '@angular/core';
+import {NgModule, ErrorHandler, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
 import {MyApp} from './app.component';
 
 import {Hikings} from '../pages/hikings/hikings';
 import {HikingDetail} from '../pages/hiking-detail/hiking-detail';
+
+// import {AgmCoreModule} from "@agm/core";
 
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
@@ -17,7 +19,10 @@ import {SplashScreen} from '@ionic-native/splash-screen';
     ],
     imports: [
         BrowserModule,
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp),
+        // AgmCoreModule.forRoot({
+        //     apiKey: 'AIzaSyBHNxJqNLAx27dFg50p_9q-0TBI1ruhlpM'
+        // })
     ],
     bootstrap: [IonicApp],
     entryComponents: [
@@ -29,6 +34,9 @@ import {SplashScreen} from '@ionic-native/splash-screen';
         StatusBar,
         SplashScreen,
         {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ],
+    schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
     ]
 })
 export class AppModule {
--- src/pages/hiking-detail/hiking-detail.ts
+++ src/pages/hiking-detail/hiking-detail.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {Hiking} from "../../model/hiking.model";
 
@@ -11,10 +11,13 @@ export class HikingDetail {
 
     public hiking: Hiking;
 
-  constructor(public params: NavParams, public navCtrl: NavController) {
-      this.hiking = this.params.get("hiking");
-  }
+    constructor(public params: NavParams, public navCtrl: NavController) {
+        this.hiking = this.params.get("hiking");
+    }
 
-
+    ionViewDidLoad() {
+        let uluru = {lat: -25.344, lng: 131.036};
+        let map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
+        new google.maps.Marker({position: uluru, map: map});
+    }
 }
-
