3bdb1eb65a0712d881460b68598647d2050fa281
Alexis Dardinier <alexis.dardinier@gmail.com>
2018-06-18 11:53:22 +0200

    Added image

--- src/pages/hiking-detail/hiking-detail.ts
+++ src/pages/hiking-detail/hiking-detail.ts
@@ -16,8 +16,8 @@ export class HikingDetail {
     }
 
     ionViewDidLoad() {
-        let uluru = {lat: -25.344, lng: 131.036};
-        let map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
-        new google.maps.Marker({position: uluru, map: map});
+        let begin = {lat: this.hiking.steps[0].latitude, lng: this.hiking.steps[0].longitude};
+        let map = new google.maps.Map(document.getElementById('map'), {zoom: 17, center: begin});
+        new google.maps.Marker({position: begin, map: map});
     }
 }
--- src/pages/hikings/hikings.ts
+++ src/pages/hikings/hikings.ts
@@ -14,9 +14,27 @@ export class Hikings {
 
     constructor(public navCtrl: NavController) {
         this.hikings = [
-            new Hiking(0, 'Randonnée de la montagne'),
-            new Hiking(1, 'Randonnée du lac'),
-            new Hiking(2, 'Randonnée du château')
+            new Hiking(
+                0,
+                'Randonnée de la montagne',
+                '8 Rue de la France',
+                'https://asal-lorient.fr/wp-content/uploads/2015/05/randonnee-pedestre-1024-1024x480.jpg',
+                [{ latitude: 48.862725, longitude: 2.287592000000018}]
+            ),
+            new Hiking(
+                1,
+                'Randonnée du lac',
+                '8 Rue de la Patrie',
+                'http://www.chamina-voyages.com/img/gammes/accompagne-0.jpg',
+                [{ latitude: 49.862725, longitude: 3.287592000000018}]
+            ),
+            new Hiking(
+                2,
+                'Randonnée de la vallée',
+                '8 Boulevard de la Nation',
+                'https://www.blog-lifestyle.fr/wp-content/uploads/2018/04/comment-preparer-sa-randonnee.jpg',
+                [{ latitude: 36.862725, longitude: 8.287592000000018}]
+            ),
         ];
     }
 
