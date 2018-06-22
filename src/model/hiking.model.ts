fb3243341e1d6541284d870b57b737fcf3bf6e7e
Alexis Dardinier <alexis.dardinier@gmail.com>
2018-06-22 13:33:49 +0200

    Added description and responsive list

--- src/model/hiking.model.ts
+++ src/model/hiking.model.ts
@@ -1,13 +1,15 @@
 export class Hiking {
     id: number;
     name: string;
+    description: string;
     beginAdress: string;
     image: string;
     steps: object;
 
-    constructor(id: number, name: string, beginAdress: string, image: string, steps: object) {
+    constructor(id: number, name: string, description: string, beginAdress: string, image: string, steps: object) {
         this.id = id;
         this.name = name;
+        this.description = description;
         this.beginAdress = beginAdress;
         this.image = image;
         this.steps = steps;
